8dd5ad6d53e78de2a1f9cfaa1b9e2b2ade76c90a
jemc300x <jemc300x@gmail.com>
2020-02-01 20:06:31 -0500

    vue-exchange

---
+++ src/components/PxButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    @click="buttonClick"
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibolt hover:text-white"
+  >
+    <beat-loader :loading="isLoading" :color="'#68d391'" :size="8" />
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PxButton",
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    buttonClick() {
+      this.$emit("click");
+    }
+  }
+}
+</script>
---
+++ src/views/Error.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl uppercase">error</h1>
+    <router-link to="/" class="mt-5 text-green-600 hover:underline">
+      Volver a la pagina de inicio
+    </router-link>
+  </div>
+</template>
